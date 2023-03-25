216733f24b670bd5a9bf7ba0ad55f6c683ccfd19
Liam Taylor <liam.taylor@yale.edu>
2023-03-25 16:01:35 -0400

    clean data ready for analysis!

--- Scripts/1_parse_data.r
+++ Scripts/1_parse_data.r
@@ -61,6 +61,7 @@ categorizeDisplayType <- function(uid) {
     if (length(maleID) > 1 | length(femID) > 1 | length(b2ID) > 1) { 
         cat(paste("\nERROR in Scripts/1_parse_data.r, categorizeDisplayType()\n",
                   "Display UID", uid, "has multiple IDs in one of the ID columns"))
+    	quit(save="no", status=1)
     }
 
     # Now get various data from the display to use in coding the type of display
@@ -89,127 +90,99 @@ categorizeDisplayType <- function(uid) {
 display_categories <- map_df(unique(data_raw$UID), categorizeDisplayType)
 write_csv(display_categories, "Output/display_categories.csv")
 
-# # A function to specify "Other" behaviors using dataset notes
-# specifyOtherBehaviors <- function(behavior, details) {
-#     # If it's not an "Other" behavior, 
-#     #   return the original behavior
-#     if (!(grepl("Other Behavior", behavior))) {
-#         return(behavior)
-#     }
+# A function to specify "Other" behaviors using dataset notes
+specifyOtherBehaviors <- function(behavior, details) {
+    # If it's not an "Other" behavior, 
+    #   return the original behavior
+    if (!(grepl("Other Behavior", behavior))) {
+        return(behavior)
+    }
 
-#     # Otherwise, return BirdID Other Behavior <details>
-#     #   (if details are specified)
-#     if (is.na(details)) {
-#         return(paste(behavior, "Unspecified"))
-#     }
-#     return(paste(behavior, details))
-# }
+    # Otherwise, return Other Behavior <details>
+    #   (if details are specified)
+    if (is.na(details)) {
+        return(paste(behavior, "Unspecified"))
+    }
+    return(paste(behavior, details))
+}
 
-# # Source behavior element dictionaries 
-# # See Data/dictionary_behaviors.r for details
-# source("Data/dictionary_behaviors.r")
+# Source behavior element dictionaries 
+# See Data/dictionary_behaviors.r for details
+source("Data/dictionary_behaviors.r")
 
-# # Organize raw data
-# # [Mutate] create col Duration (max element time) before cutting any elements 
-# # [Filter] out partial behaviors
-# # [Filter] out movement-only behaviors not used in analyses
-# # [Filter] out tracking-type elements (Start, End, Female On, Female Off)
-# # [Mutate] specify "other" behaviors using dataset notes where applicable 
-# # [Filter] out "Other"-type behaviors when known or specified not to constitute direct dance display elements
-# #          NOTE these include vocalizations, gardening, and wing-flashing
-# # [Mutate] replace original behavioral descriptions with abbreviations 
-# # [Mutate] create single-column abbreviate col 
-# data_clean_long <- data_raw |>
-#                 group_by(UID) |> mutate(Duration = max(Time)) |> ungroup() |>
-#                 filter(!(Behavior %in% behaviors_cut_partial)) |>
-#                 filter(!(Behavior %in% behaviors_cut_movement)) |>
-#                 filter(!(Behavior %in% behaviors_cut_tracking)) |>
-#                 mutate(Behavior = map2_chr(Behavior, MaleOtherBeh1, specifyOtherBehaviors)) |>
-#                 filter(!(Behavior %in% behaviors_cut_other)) |>
-#                 mutate(Behavior = behaviorCodes_original[Behavior]) |>
-#                 mutate(BehaviorCode = behaviorCodes_short[Behavior])
+# Organize raw data
+# [Left join] Displays to get display categories
+# [Filter] out MULTI displays (should retain SOLO, AUDI, COP only)
+# [Filter] out partial behaviors
+# [Filter] out movement-only behaviors not used in analyses
+# [Filter] out tracking-type elements (Start, End, Female On, Female Off)
+# [Mutate] specify "other" behaviors using dataset notes where applicable 
+# [Filter] out "Other"-type behaviors when known or specified not to constitute direct dance display elements
+#          NOTE these include vocalizations, gardening, and wing-flashing
+# [Mutate] replace original behavioral descriptions with abbreviations 
+# [Mutate] create single-column abbreviate col 
+data_clean_long <- data_raw |>
+                left_join(select(display_categories, UID, Category), by="UID") |>
+                filter(Category != "MULT") |>
+                filter(!(Behavior %in% behaviors_cut_partial)) |>
+                filter(!(Behavior %in% behaviors_cut_movement)) |>
+                filter(!(Behavior %in% behaviors_cut_tracking)) |>
+                mutate(Behavior = map2_chr(Behavior, MaleOtherBeh1, specifyOtherBehaviors)) |>
+                filter(!(Behavior %in% behaviors_cut_other)) |>
+                mutate(BehaviorShort = behavior_short[Behavior]) |>
+                mutate(BehaviorCode = behavior_code[BehaviorShort])
 
-# # Produce clean dataset with display element strings
-# # These steps to include only male aesthetic displays
-# #	in terms of the display
-# # 1. FILTER out:
-# #	 "Start", "End", "OthF", "FOn", and "Fff" elements
-# # 2. MUTATE new bool column with coded display elements
-# # 3. SUMMARIZE displays by collapsing elements to strings
-# # 4. MUTATE new bool column for Multiple males present (i.e., has B2 display elements)
-# # 5. MUTATE new bool column for Female present (i.e., FemID not NA)
-# # 6. MUTATE new chr column for coding of display types:
-# #		SOL bouts have one Male, no female present
-# #		MUL bouts have multiple Males, no female present
-# #		AUD bouts have female present, but no Cop 
-# #		COP bouts have female present, and Cop
-# # 7. MUTATE DisplayType column from Character -> Factors
+# Confirm that all the behavioral elements are in the dictionary
+missingCodes <- data_clean_long |>
+			 filter(is.na(BehaviorShort) | is.na(BehaviorCode)) |>
+             select(UID, Time, Male1ID, Behavior, BehaviorShort, BehaviorCode)
+if (nrow(missingCodes) > 0) {
+	cat("\n\nERROR in Scripts/1_parse_data.r:\nYour behaviorCode dictionary does not include all behaviors.\n\nQUITTING\n\n")
+    print(missingCodes)
+    quit(save="no", status=1)
+}
 
-# # Which elements are excluded?
-# excludedElements <- c("Start", "End", "FOn", "Fff", "AttC")
+# Custom function to end uncoded behavior strings at cop
+# Returns original string if no Cop element present
+cutDisplayAtCop <- function(s, coded=FALSE) {
+	# If the behaviors are coded,
+	#	we're cutting at behaviorCods["Cop"] (should be "U")
+	# If the behaviors are uncoded, 
+	#	we're cutting at "Cop"
+	if (coded) {
+		copElement <- behavior_code["Cop"]
+		ret <- strsplit(s, copElement)[[1]][1]
+	} else {
+		copElement <- ";Cop"
+		ret <- strsplit(s, copElement)[[1]][1]
+	}
+	return(ret)
+}
 
-# # Custom function to end uncoded behavior strings at cop
-# # Returns original string if no Cop element present
-# cutAtCop <- function(s, coded=FALSE) {
-# 	# If the behaviors are coded,
-# 	#	we're cutting at behaviorCods["Cop"] (should be "U")
-# 	# If the behaviors are uncoded, 
-# 	#	we're cutting at "Cop"
-# 	if (coded) {
-# 		copElement <- behaviorCodes["Cop"]
-# 		ret <- strsplit(s, copElement)[[1]][1]
-# 	} else {
-# 		copElement <- ";Cop"
-# 		ret <- strsplit(s, copElement)[[1]][1]
-# 	}
-# 	return(ret)
-# }
+# Custom function to calculate display duration
+#   NOTE calculated from Raw data, including all cut elements
+#   NOTE cuts duration off at Cop, when relevant
+displayDuration <- function(uid, coded=FALSE) {
+    display <- data_raw |>
+            filter(UID == uid)
 
-# # Custom function to classify displays
-# #		SOL bouts have one Male, no female present
-# #		MUL bouts have multiple Males, no female present
-# #		AUD bouts have female present, but no Cop 
-# #		COP bouts have female present, and Cop
-# getDisplayType <- function(hasMultipleMales, hasFemale, hasCopulation) {
-# 	if (!hasMultipleMales & !hasFemale & !hasCopulation) {
-# 		return("SOL")
-# 	} else if (hasMultipleMales & !hasFemale & !hasCopulation) {
-# 		return("MUL")
-# 	} else if (!hasMultipleMales & hasFemale & !hasCopulation) {
-# 		return("AUD")
-# 	} else if (!hasMultipleMales & hasFemale & hasCopulation) {
-# 		return("COP")
-# 	}
+    duration <- 0
+    for (r in 1:nrow(display)) {
+        duration <- display$Time[r]
+        behavior <- display$Behavior[r]
+        if (behavior == "Copulation") { return(duration) }
+    }
+    return(duration)
+}
 
-# 	# TODO
-# 	# all other combinations might erroneous -- code them as "CHECK" 
-# 	return("CHECK")
-# } 
+data_clean_wide <- data_clean_long |>
+		        group_by(UID, Category, ObsDate, Log, Male1ID, FemID, Bird2ID) |>
+		        summarise(DisplayShort = paste(BehaviorShort, collapse=";"), 
+                          DisplayCode = paste(BehaviorCode, collapse=""), 
+                          .groups="keep") |>	        
+                mutate(DisplayShort = map_chr(DisplayShort, ~cutDisplayAtCop(., coded=FALSE)),
+		   		       DisplayCode = map_chr(DisplayCode, ~cutDisplayAtCop(., coded=TRUE))) |>
+                mutate(Duration = map_dbl(UID, displayDuration)) |>
+                ungroup()
 
-
-# # ERROR CHECK does your dictionary contain codes for all behaviors?
-# missingCodes <- data_raw |>
-# 			 filter(!(Behavior %in% names(behaviorCodes)))
-# if (nrow(missingCodes) > 0) {
-# 	cat("\n\nERROR in Scripts/1_parse_data.r:\nYour behaviorCode dictionary does not include all behaviors.\n\nQUITTING\n\n")
-# 	quit(save="no", status=1)
-# }
-
-# data_clean <- data_raw |>
-# 		   filter(!(Behavior %in% excludedElements)) |>
-# 		   mutate(BehaviorCode = map_chr(Behavior, ~ behaviorCodes[.])) |>
-# 		   group_by(UID, ObsDate, Log, Male1ID, FemID, Bird2ID) |>
-# 		   summarize(Duration = max(Time) - min(Time),
-#                      Display = paste(Behavior, collapse=";"),
-# 		   		  	 DisplayCode = paste(BehaviorCode, collapse=""),
-# 		    		 .groups="keep") |>
-# 		   mutate(hasMultipleMales = grepl("B2AL", Display) |
-# 		   						     grepl("B2Bw", Display) |
-# 		   							 grepl("B2Nk", Display)) |>
-# 		   mutate(hasFemale = !is.na(FemID)) |>
-# 		   mutate(hasCopulation = map_lgl(Display, ~grepl("Cop", .))) |>
-# 		   mutate(Display = map_chr(Display, ~cutAtCop(., coded=FALSE)),
-# 		   		  DisplayCode = map_chr(DisplayCode, ~cutAtCop(., coded=TRUE))) |>
-# 		   mutate(DisplayType = pmap_chr(list(hasMultipleMales, hasFemale, hasCopulation), 
-# 		   		  						 getDisplayType))
-# write_csv(data_clean, "Data/data_clean.csv")
+write_csv(data_clean_wide, "Data/data_clean.csv")
